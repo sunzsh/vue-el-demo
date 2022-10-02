c7141251b50a3a8670d59c837c398727bc1cd0e3
sunzsh <cn.xiaoshan@gmail.com>
2022-10-02 10:54:22 +0800

    dynamiccss.vue增加el-select

--- src/views/dynamiccss.vue
+++ src/views/dynamiccss.vue
@@ -3,8 +3,19 @@
   <div>
 
     <div :style="declareBgColor" class="bg">
-      test
+        test
     </div>
+    <br/>
+
+    <el-select :style="declareBgColor" class="bg2" :popper-append-to-body="false" v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <br/>
 
     <input v-model="bgColor" />
 
@@ -15,7 +26,24 @@
 export default {
   data() {
     return {
-      bgColor: 'blue'
+      bgColor: 'blue',
+       options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
     }
   },
   computed: {
@@ -29,8 +57,14 @@ export default {
 <style scoped>
 
 .bg {
-  
   background-color: var(--bgColor);
+}
+
+.bg2 >>> .el-popper .el-scrollbar {
+  background-color: var(--bgColor);
+}
+.bg2 >>> .popper__arrow::after {
+  border-bottom-color: var(--bgColor);
 }
 
 
